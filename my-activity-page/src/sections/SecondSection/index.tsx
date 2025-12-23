84c129903249527e0e0afbb2eb28c0a78565750a
Jocelyn-Bruni <record.life.start@gmail.com>
2025-12-24 00:36:25 +0800

    完成第二部分基礎樣式

--- my-activity-page/src/sections/SecondSection/index.tsx
+++ my-activity-page/src/sections/SecondSection/index.tsx
@@ -1,9 +1,42 @@
 import{FC} from "react";
+import CartoonImage from '../../image/cartoon.jpg';
+import MovieImage from '../../image/movie.png';
+import LifeImage from '../../image/life.jpg';
+import FoodImage from '../../image/food.jpg';
+
+import sytles from './styles.module.scss';
+
 
 const SecondSection:FC = () => {
     return (
-        <div>
-            Tabs + Tab Content
+        <div className={sytles.secondSection}>
+            {/*Tabs */}
+            <ul>
+                <li>動畫</li>
+                <li>美食</li>
+                <li>電影</li>
+                <li>生活</li>
+            </ul>
+            
+            {/*Tab Content */}
+            <div>
+                <section>
+                    <h2>動畫</h2>
+                    <img src={CartoonImage} alt='cartoon'/>
+                </section>
+                <section>
+                    <h2>美食</h2>
+                    <img src={FoodImage} alt='Food'/>
+                </section>
+                <section>
+                    <h2>電影</h2>
+                    <img src={MovieImage} alt='Movie'/>
+                </section>
+                <section>
+                    <h2>生活</h2>
+                    <img src={LifeImage} alt='Life'/>
+                </section>
+            </div>
         </div>
     )
 }
