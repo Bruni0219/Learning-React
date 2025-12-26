be72457ca1aeedaaa04ffb21b2ac24ee746682bd
林佳嫻 <record.life.start@gmail.com>
2025-12-26 18:58:01 +0800

    有bug

--- my-activity-page/src/sections/SecondSection/index.tsx
+++ my-activity-page/src/sections/SecondSection/index.tsx
@@ -1,4 +1,4 @@
-import{FC,useState} from "react";
+import{FC,useEffect,useRef,useState} from "react";
 import CartoonImage from '../../image/cartoon.jpg';
 import MovieImage from '../../image/movie.png';
 import LifeImage from '../../image/life.jpg';
@@ -27,12 +27,40 @@ const tabs=[
 ];
 const SecondSection:FC = () => {
     const [activeTab, setActiveTab] = useState<string>('cartoon');
+    const [isFixed,setIsFixed] = useState<boolean>(false);
+    const secondSectionRef = useRef<HTMLDivElement>(null);
+
+    const activate = (key:string) => {
+        setActiveTab(key);
+        const tabContentEL = document.querySelector(`[data-id="${key}"]`);
+        if (tabContentEL){
+            tabContentEL.scrollIntoView({behavior: "smooth"});
+        }
+
+    }
+    
+    const onScroll = () =>{
+        if (secondSectionRef.current){
+            const {top} =  secondSectionRef.current.getBoundingClientRect();
+            setIsFixed(top <= 0);
+        }
+
+    useEffect(() =>{
+        window.addEventListener('scroll',onScroll);
+
+        return() =>{
+            window.removeEventListener('scroll',onScroll);
+        };       
+        },[]);      
+
+    }
+
     return (
-        <div className={styles.secondSection}>
+        <div className={styles.secondSection} ref = {secondSectionRef}>
             {/*Tabs */}
-            <ul>
+            <ul className={classNames({[styles.isFixed]:isFixed})}>
                 {tabs.map(tab =>(
-                    <li key={tab.key} onClick={()=>setActiveTab(tab.key)}>  
+                    <li key={tab.key} onClick={()=>activate(tab.key)}>  
                         <span>{tab.title}</span>                        
                         <span className={classNames(styles.line,{[styles.visible]:activeTab===tab.key})} />
                     </li>
@@ -41,22 +69,12 @@ const SecondSection:FC = () => {
             
             {/*Tab Content */}
             <div>
-                <section>
-                    <h2>動畫</h2>
-                    <img src={CartoonImage} alt='cartoon'/>
-                </section>
-                <section>
-                    <h2>美食</h2>
-                    <img src={FoodImage} alt='Food'/>
-                </section>
-                <section>
-                    <h2>電影</h2>
-                    <img src={MovieImage} alt='Movie'/>
-                </section>
-                <section>
-                    <h2>生活</h2>
-                    <img src={LifeImage} alt='Life'/>
-                </section>
+                {tabs.map(tab =>(
+                    <section data-id ={tab.key}>
+                        <h2>{tab.title}</h2>
+                        <img src={CartoonImage} alt={tab.key}/>
+                    </section>
+                ))}                
             </div>
         </div>
     )
